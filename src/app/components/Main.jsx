4d34a0605962afbcefdac414293ef2bb95676dd4
enemuo <francenemuo@gmail.com>
2023-11-29 19:40:08 +0100

    fix: change landing page image

--- src/app/components/Main.jsx
+++ src/app/components/Main.jsx
@@ -11,10 +11,12 @@ export default function Main() {
         <Image src='/media/books.jpg' alt='books' width={600} height={500}/>
 
         <Image src='/media/morebooks.jpg' alt='books' width={600} height={500}/>
+        
+        <Image src='/media/morebooks.jpg' alt='books' width={600} height={500}/>
 
         <Image src='https://images.unsplash.com/photo-1456513080510-7bf3a84b82f8?q=80&w=1373&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D' alt='books' width={600} height={500}/>
 
-        <Image src={pic} alt='books' width={600} height={500}/>
+        {/* <Image src={pic} alt='books' width={600} height={500}/> */}
         </section>
     </div>
   )
